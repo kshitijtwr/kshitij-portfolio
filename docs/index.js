68d3a32598e4d75f8158f90e0c743aa0d118a3a8
kshitijtwr <tiwarikshitij@gmail.com>
2025-08-16 15:09:39 +0530

    Deploy to docs for GitHub Pages

--- docs/index.js
+++ docs/index.js
@@ -118,7 +118,7 @@ var vite_config_default = defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: "dist",
     emptyOutDir: true
   },
   server: {
