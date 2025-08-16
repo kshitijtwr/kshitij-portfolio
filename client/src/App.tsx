7736d5045f8471ad868f1209a712b885a2b0ec49
kshitijtwr <tiwarikshitij@gmail.com>
2025-08-16 16:44:57 +0530

    new changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,24 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
-import NotFound from "@/pages/not-found";
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
-
-function Router() {
-  return (
-    <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-  );
-}
+import { Route, Switch, HashRouter } from 'wouter';
+import { queryClient } from './lib/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from '@/components/ui/toaster';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import Home from '@/pages/home';
+import NotFound from '@/pages/not-found';
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        {/* Wrap your routes in HashRouter */}
+        <HashRouter>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
