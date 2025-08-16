857ee07c3804301fad06bb9ae173cc4d8530acf7
kshitijtwr <tiwarikshitij@gmail.com>
2025-08-16 16:48:37 +0530

    main.js file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Route, Switch, Router } from 'wouter';
-import { useHashLocation } from 'wouter';
-import { queryClient } from './lib/queryClient';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from '@/components/ui/toaster';
-import { TooltipProvider } from '@/components/ui/tooltip';
-import Home from '@/pages/home';
-import NotFound from '@/pages/not-found';
+import React from "react";
+import { Route, Switch, Router } from "wouter";
+import { useHashLocation } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/home";
+import NotFound from "@/pages/not-found";
 
 function App() {
   return (
