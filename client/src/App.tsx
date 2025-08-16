f63026f5c96b99520071a01f37145224d8837e29
kshitijtwr <tiwarikshitij@gmail.com>
2025-08-16 16:51:03 +0530

    deploy changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,29 @@
-import React from "react";
-import { Route, Switch, Router } from "wouter";
-import { useHashLocation } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+
+function Router() {
+  return (
+    <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+  );
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router hook={useHashLocation}>
-          <Switch>
-            <Route path="/" component={Home} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </Router>
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
