4a1e6f3eaa23494fd22af189e43889341ccdaf0a
kshitijtwr <tiwarikshitij@gmail.com>
2025-08-16 16:54:30 +0530

    app file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,16 +5,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
-import React from 'react';
-import { HashRouter } from 'react-router-dom';
 
 function Router() {
   return (
-    <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
